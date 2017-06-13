d9024a7201e294b1b8b33bd7023596475f40bd6e
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-13 14:51:58 +0200

    Add draw test

--- src/test/java/Board/BoardTest.java
+++ src/test/java/Board/BoardTest.java
@@ -1,23 +1,26 @@
 package Board;
 
+import enums.Seed;
+import exceptions.CellNotEmptyException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import Cell.Cell;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        this.board = new Board();
+    }
 
     @Nested
     class InitTest {
-        private Board board;
-
-        @BeforeEach
-        void setUp() {
-            this.board = new Board();
-        }
 
         @Test
         void testIfConstructorCreateEmptyCellsArray() {
@@ -33,5 +36,19 @@ public class BoardTest {
         }
     }
 
+    @Nested
+    class MethodsTests {
+        @BeforeEach
+        void setUp() {
+            board.init();
+        }
 
+        void testIfIsDrawReturnsTrueWhenNoValidMoveLeft() throws CellNotEmptyException {
+            Seed cross = Seed.CROSS;
+            for (Cell cell: board.getCells()) {
+                cell.setContent(cross);
+            }
+            assertEquals(true, board.isDraw());
+        }
+    }
 }
