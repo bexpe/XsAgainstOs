4c94375dacfd9d118c8bafb60d79833f6ff62ccb
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-13 11:34:18 +0200

    Add constructor test to CellTest

--- src/test/java/Cell/CellTest.java
+++ src/test/java/Cell/CellTest.java
@@ -1,7 +1,12 @@
 package Cell;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
-
+    @Test
+    void testIfConstructorThrowsExceptionWhenRowLT0() {
+        assertThrows(IllegalArgumentException.class, () -> new Cell(-1, 0));
+    }
 }
