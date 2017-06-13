d4cc638cf9b4e41d9046ef0fa31e15e883904b0b
bexpe <beata.k.puzio@gmail.com>
2017-06-13 11:57:29 +0200

    Add test if default content is empty

--- src/test/java/Cell/CellTest.java
+++ src/test/java/Cell/CellTest.java
@@ -1,12 +1,34 @@
 package Cell;
 
+import enums.Seed;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
-    @Test
-    void testIfConstructorThrowsExceptionWhenRowLT0() {
-        assertThrows(IllegalArgumentException.class, () -> new Cell(-1, 0));
+    @Nested
+    class ConstructorTests {
+        @Test
+        void testIfConstructorThrowsExceptionWhenRowLT0() {
+            assertThrows(IllegalArgumentException.class, () -> new Cell(-1, 0));
+        }
     }
+
+    @Nested
+    class MethodsTests {
+        private Cell cell;
+
+        @BeforeEach
+        void setUp() {
+            this.cell = new Cell(2,3);
+        }
+
+        @Test
+        void testIfDefaultContentIsEmpty() {
+            assertEquals(Seed.EMPTY, cell.getContent());
+        }
+    }
+
 }
