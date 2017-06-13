47c378e0059cb820f4f38eef646ecdb537155b99
bexpe <beata.k.puzio@gmail.com>
2017-06-13 11:40:26 +0200

    Add constructor to Cell

--- src/main/java/Cell/Cell.java
+++ src/main/java/Cell/Cell.java
@@ -1,4 +1,14 @@
 package Cell;
 
 public class Cell {
+    int row;
+    int col;
+
+    public Cell(int row, int col) throws IllegalArgumentException {
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException("row or col cant be less than 0");
+        }
+        this.row = row;
+        this.col = col;
+    }
 }
