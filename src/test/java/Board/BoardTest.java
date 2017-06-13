eec4b8304779f88fec1a2a2733d8900e302dc017
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-13 14:16:47 +0200

    Fix assertEquals typo

--- src/test/java/Board/BoardTest.java
+++ src/test/java/Board/BoardTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BoardTest {
 
     @Test
     void testIfConstructorCreateEmptyCellsArray() {
         Board board = new Board();
-        assert equals(0, board.cells.size());
+        assertEquals(0, board.getCells().size());
     }
 
 }
