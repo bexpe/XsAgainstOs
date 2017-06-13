a189b165d9538247d837ebe538f7841d3dc00216
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-13 14:17:12 +0200

    Add Board class with init method

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -1,4 +1,23 @@
 package Board;
 
+import Cell.Cell;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private List<Cell> cells = new ArrayList<>();
+
+    public List<Cell> getCells() {
+        return cells;
+    }
+
+    public void init() {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                Cell cell = new Cell(i, j);
+                cells.add(cell);
+            }
+        }
+    }
 }
