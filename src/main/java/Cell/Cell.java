4ed1a5947a5630ccc1f1ce2b9b10b89652e5017c
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-13 11:59:55 +0200

    Add getters to Cell

--- src/main/java/Cell/Cell.java
+++ src/main/java/Cell/Cell.java
@@ -1,8 +1,11 @@
 package Cell;
 
+import enums.Seed;
+
 public class Cell {
-    int row;
-    int col;
+    private Seed content = Seed.EMPTY;
+    private int row;
+    private int col;
 
     public Cell(int row, int col) throws IllegalArgumentException {
         if (row < 0 || col < 0) {
@@ -10,5 +13,17 @@ public class Cell {
         }
         this.row = row;
         this.col = col;
+    }
+
+    public Seed getContent() {
+        return content;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
     }
 }
