edf30c16bcc68b6f4fc8c66b888298defbf61075
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-14 10:08:02 +0200

    Change array list to hash map and refactor code

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -3,15 +3,15 @@ package Board;
 import Cell.Cell;
 import enums.Seed;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Board {
-    private List<Cell> cells = new ArrayList<>();
+    private Map<String, Cell> cells = new HashMap<>();
     private final Integer MAX_ROW_INDEX = 2;
     private final Integer MAX_COL_INDEX = 2;
 
-    public List<Cell> getCells() {
+    public Map<String, Cell> getCells() {
         return cells;
     }
 
@@ -19,18 +19,17 @@ public class Board {
         for (Integer row = 0; row <= MAX_ROW_INDEX; row++) {
             for (Integer col = 0; col <= MAX_COL_INDEX; col++) {
                 Cell cell = new Cell(row, col);
-                cells.add(cell);
+                cells.put(row + " " + col, cell);
             }
         }
     }
 
     public Boolean isDraw() {
-        for (Cell cell: cells) {
-            if (cell.getContent() == Seed.EMPTY) {
+        for (Map.Entry<String, Cell> pair : cells.entrySet()) {
+            if (pair.getValue().getContent() == Seed.EMPTY) {
                 return false;
             }
         }
         return true;
     }
-
 }
