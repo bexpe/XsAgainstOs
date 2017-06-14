fc09d836d4bcc8194ed7f9ee4cdbb5f744ad9ba7
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-14 11:18:20 +0200

    Add BoardChecker class

---
+++ src/main/java/Board/BoardChecker.java
@@ -0,0 +1,23 @@
+package Board;
+
+import Cell.Cell;
+
+import java.util.Map;
+
+class BoardChecker {
+    Boolean hasWon(Map<String, Cell> cells) {
+        return checkVertical() || checkHorizontal() || checkDiagonal();
+    }
+
+    private Boolean checkVertical() {
+
+    }
+
+    private Boolean checkHorizontal() {
+
+    }
+
+    private Boolean checkDiagonal() {
+
+    }
+}
