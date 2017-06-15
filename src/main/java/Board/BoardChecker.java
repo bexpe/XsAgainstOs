fdc43a5e826a1c61f76f1418b016c216c7601af7
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-15 20:20:43 +0200

    Remake BoardChecker

--- src/main/java/Board/BoardChecker.java
+++ src/main/java/Board/BoardChecker.java
@@ -3,47 +3,40 @@ package Board;
 import Cell.Cell;
 import Cell.Seed;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 class BoardChecker {
 
 
     Boolean hasWon(Board board, Map<String, Cell> cells) {
-        return checkVertical(board, cells) || checkHorizontal(board, cells) || checkLeftDiagonal(board, cells) || checkRightDiagonal(board, cells);
+        return checkVertical(board) || checkHorizontal(board) || checkLeftDiagonal(board, cells) || checkRightDiagonal(board, cells);
     }
 
-    private Boolean checkHorizontal(Board board, Map<String, Cell> cells) {
-        for (Integer row = 1; row <= board.getMAX_ROW_INDEX(); row++) {
-            Integer counter = 1;
-            Cell currentCell = cells.get(row + " " + 1);
-            for (Integer col = 2; col <= board.getMAX_COL_INDEX(); col++) {
-                if (currentCell.getContent() != cells.get(row + " " + col).getContent() ||
-                        currentCell.getContent() == Seed.EMPTY) {
-                    break;
-                } else {
-                    counter++;
-                }
-            }
-            if (counter == board.getMAX_ROW_INDEX()) {
+    private Boolean checkHorizontal(Board board) {
+        List<Seed> cellsContent = board.getCellsContentAsSeed();
+
+        for (Integer index = 0; index < cellsContent.size(); index += board.getMAX_COL_INDEX()) {
+            if (new HashSet<>(cellsContent.subList(index, index + board.getMAX_COL_INDEX())).size() == 1 &&
+                    cellsContent.get(index) != Seed.EMPTY) {
                 return true;
             }
         }
         return false;
     }
 
-    private Boolean checkVertical(Board board, Map<String, Cell> cells) {
-        for (Integer col = 1; col <= board.getMAX_ROW_INDEX(); col++) {
-            Integer counter = 1;
-            Cell currentCell = cells.get(1 + " " + col);
-            for (Integer row = 2; row <= board.getMAX_ROW_INDEX(); row++) {
-                if (currentCell.getContent() != cells.get(row + " " + col).getContent() ||
-                        currentCell.getContent() == Seed.EMPTY) {
-                    break;
-                } else {
-                    counter++;
-                }
+    private Boolean checkVertical(Board board) {
+        List<Seed> cellsContent = board.getCellsContentAsSeed();
+
+        for (Integer colIndex = 0; colIndex < board.getMAX_COL_INDEX(); colIndex++) {
+            List<Seed> colList = new ArrayList<>();
+            for (Integer rowIndex = 0; rowIndex < cellsContent.size(); rowIndex += board.getMAX_COL_INDEX()) {
+                colList.add(cellsContent.get(rowIndex + colIndex));
             }
-            if (counter == board.getMAX_ROW_INDEX()) {
+            if (new HashSet<>(colList).size() == 1 &&
+                    !colList.contains(Seed.EMPTY)) {
                 return true;
             }
         }
@@ -51,26 +44,24 @@ class BoardChecker {
     }
 
     private Boolean checkLeftDiagonal(Board board, Map<String, Cell> cells) {
-        Cell cellLeftTop = cells.get("1 1");
-        Cell cellCenter = cells.get("2 2");
-        Cell cellRightBottom = cells.get("3 3");
-        if (cellLeftTop.getContent() == cellCenter.getContent() &&
-                cellCenter.getContent() == cellRightBottom.getContent() &&
-                cellCenter.getContent() != Seed.EMPTY) {
-            return true;
+        List<Seed> cellsContent = board.getCellsContentAsSeed();
+        List<Seed> diagonalList = new ArrayList<>();
+
+        for (Integer index = 0; index < cellsContent.size(); index += board.getMAX_COL_INDEX() + 1) {
+            diagonalList.add(cellsContent.get(index));
         }
-        return false;
+        return new HashSet<>(diagonalList).size() == 1 &&
+                !diagonalList.contains(Seed.EMPTY);
     }
 
     private Boolean checkRightDiagonal(Board board, Map<String, Cell> cells) {
-        Cell cellRightTop = cells.get("1 3");
-        Cell cellCenter = cells.get("2 2");
-        Cell cellLeftBottom = cells.get("3 1");
-        if (cellLeftBottom.getContent() == cellCenter.getContent() &&
-                cellCenter.getContent() == cellRightTop.getContent() &&
-                cellCenter.getContent() != Seed.EMPTY) {
-            return true;
+        List<Seed> cellsContent = board.getCellsContentAsSeed();
+        List<Seed> diagonalList = new ArrayList<>();
+
+        for (Integer index = board.getMAX_COL_INDEX() - 1; index < cellsContent.size() - 1; index += (board.getMAX_COL_INDEX() - 1)) {
+            diagonalList.add(cellsContent.get(index));
         }
-        return false;
+        return new HashSet<>(diagonalList).size() == 1 &&
+                !diagonalList.contains(Seed.EMPTY);
     }
 }
