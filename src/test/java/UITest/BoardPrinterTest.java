97a7980626dcc1675eb07d988b38944e1a658f4d
bexpe <beata.k.puzio@gmail.com>
2017-06-15 19:51:39 +0200

    Add BoardPrinter test

---
+++ src/test/java/UITest/BoardPrinterTest.java
@@ -0,0 +1,31 @@
+package UITest;
+
+import UI.BoardPrinter;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoardPrinterTest {
+    @Test
+    void testPrintBoard() {
+        BoardPrinter newBoard = new BoardPrinter();
+        List<String> cellsContent = new ArrayList<>();
+        String[] seeds = {"X"," "," "," "," ","X"," "," ","X"};
+
+        for(int i =  0; i < seeds.length; i++){
+            cellsContent.add(seeds[i]);
+        }
+        newBoard.printBoard(cellsContent);
+        assertEquals(newBoard.getSb().toString(),
+                "+---+---+---+---+\n" +
+                "| X  |     |    |\n" +
+                "-----------------\n" +
+                "|    |     |  X |\n" +
+                "-----------------\n" +
+                "|    |     |  X |\n" +
+                "+---+---+---+---+" );
+    }
+}
