51765f4eeeaf6bb92449290c3ab4ad4e5e05c782
bexpe <beata.k.puzio@gmail.com>
2017-06-15 17:19:13 +0200

    Add all tests to updateGameState

--- src/test/java/Game/GameTest.java
+++ src/test/java/Game/GameTest.java
@@ -16,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class GameTest {
     Game game;
     Seed cross = Seed.CROSS;
+    Seed nought = Seed.NOUGHT;
 
     @BeforeEach
     void setUp() {
@@ -46,7 +47,7 @@ public class GameTest {
     }
 
     @Test
-    void testUpdateGameState() throws CellNotEmptyException {
+    void testUpdateGameStateWhenSeedCross() throws CellNotEmptyException {
         String coordinatesCenterTop = "1 2";
         String coordinatesCenter = "2 2";
         String coordinatesCenterBottom = "3 2";
@@ -55,5 +56,41 @@ public class GameTest {
         game.getBoard().getCells().get(coordinatesCenterBottom).setContent(cross);
         game.updateGameState();
         assertEquals("CROSS_WON", game.getGameState().toString());
+    }
+
+    @Test
+    void testUpdateGameStateWhenSeedNought () throws CellNotEmptyException {
+        String coordinatesCenterTop = "1 2";
+        String coordinatesCenter = "2 2";
+        String coordinatesCenterBottom = "3 2";
+        game.getBoard().getCells().get(coordinatesCenterTop).setContent(nought);
+        game.getBoard().getCells().get(coordinatesCenter).setContent(nought);
+        game.getBoard().getCells().get(coordinatesCenterBottom).setContent(nought);
+        game.updateGameState();
+        assertEquals("NOUGHT_WON", game.getGameState().toString());
+    }
+
+    @Test
+    void testUpdateGameStateWhenDraw () throws CellNotEmptyException {
+        String coordinatesCenterTop = "1 2";
+        String coordinatesLeftTop = "1 1";
+        String coordinatesRightTop = "1 3";
+        String coordinatesCenter = "2 2";
+        String coordinatesRightCenter = "2 1";
+        String coordinatesLeftCenter = "2 3";
+        String coordinatesCenterBottom = "3 2";
+        String coordinatesLeftBottom = "3 1";
+        String coordinatesRightBottom = "3 3";
+        game.getBoard().getCells().get(coordinatesLeftTop).setContent(cross);
+        game.getBoard().getCells().get(coordinatesCenterTop).setContent(nought);
+        game.getBoard().getCells().get(coordinatesRightTop).setContent(nought);
+        game.getBoard().getCells().get(coordinatesLeftCenter).setContent(nought);
+        game.getBoard().getCells().get(coordinatesCenter).setContent(cross);
+        game.getBoard().getCells().get(coordinatesRightCenter).setContent(cross);
+        game.getBoard().getCells().get(coordinatesLeftBottom).setContent(cross);
+        game.getBoard().getCells().get(coordinatesCenterBottom).setContent(nought);
+        game.getBoard().getCells().get(coordinatesRightBottom).setContent(nought);
+        game.updateGameState();
+        assertEquals("DRAW", game.getGameState().toString());
     }
 }
